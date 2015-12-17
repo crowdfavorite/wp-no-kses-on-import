6131992700e40a2fb2f2d164a0db6a4ac2d3b121
lb1programmer <luke@linuxpc>
2015-12-16 23:00:31 -0800

    Improved security

--- cf-no-kses-on-import.php
+++ cf-no-kses-on-import.php
@@ -2,12 +2,20 @@
 /*
 Plugin Name: CF No KSES on Import
 Plugin URI: 
-Description: Removes all KSES filtering on import.  Only enable for single import (of content you absolutely trust), then deactivate.
-Version: 1.0.1
+Description: Removes all KSES filtering on import.  Only enable for import (of content you absolutely trust), then deactivate.
+Version: 1.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
 
 // remove all kses filters for import
-add_action('import_start', create_function('', 'kses_remove_filters();'));
-?>
+add_action('import_start', 'cfNoKsesOnImport');
+
+function cfNoKsesOnImport()
+{
+	//Only if administrator:
+	if( current_user_can('manage_options') )
+	{
+		kses_remove_filters();
+	}
+}
